863e2400593efcb67edbe28a5350c3c00683fc55
Pixel-ten <gridlinetech10@gmail.com>
2026-01-30 19:59:27 +0530

    new project added

---
+++ script.js
@@ -0,0 +1,12 @@
+var miniNav = document.getElementById("mini_nav");
+var navBtncontainer = document.getElementById("nav_btns");
+var navigation_container = document.getElementById("navigation_container");
+miniNav.addEventListener("click",function(){  
+    navigation_container.style.position = "relative";
+    navBtncontainer.style.display = "flex";
+    navBtncontainer.style.flexDirection = "column";
+    navBtncontainer.style.position = "absolute";
+    navBtncontainer.style.top = "0px";
+    navBtncontainer.style.left = "0px";
+    
+});
