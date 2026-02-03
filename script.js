6c4ac3e27969707fdbb75d09abcda10176bfd595
Pixel-ten <gridlinetech10@gmail.com>
2026-02-03 11:37:38 +0530

    my commit

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var miniNav = document.getElementById("mini_nav");
 var navBtncontainer = document.getElementById("nav_btns");
-var navigation_container = document.getElementById("navigation_container");
+var navigationcontainer = document.getElementById("navigation_container");
 miniNav.addEventListener("click",function(){  
-    navigation_container.style.position = "relative";
+    navigationcontainer.style.position = "relative";
     navBtncontainer.style.display = "flex";
     navBtncontainer.style.flexDirection = "column";
     navBtncontainer.style.position = "absolute";
