3ff31565e2432c38d2d3bc073e4d8aec716f3391
Pixel-ten <gridlinetech10@gmail.com>
2026-02-03 11:43:14 +0530

    zx

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var miniNav = document.getElementById("mini_nav");
 var navBtncontainer = document.getElementById("nav_btns");
 var navigationcontainer = document.getElementById("navigation_container");
 miniNav.addEventListener("click",function(){  
-    navigationcontainer.style.position = "relative";
+    // navigationcontainer.style.position = "relative";
     navBtncontainer.style.display = "flex";
     navBtncontainer.style.flexDirection = "column";
     navBtncontainer.style.position = "absolute";
